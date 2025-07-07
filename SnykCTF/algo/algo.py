d4c1ac57d2f0f10c2a7d302d69b2fc895afe96f2
XinshengZhu <xinsheng.zhu.ny@gmail.com>
2025-07-07 14:51:21 +0800

    Update SnykCTF

--- SnykCTF/algo/algo.py
+++ SnykCTF/algo/algo.py
@@ -1,34 +1,24 @@
 from pwn import *
 
-context.arch = 'amd64'
-context.log_level = 'debug'
-context.terminal = ['tmux', 'splitw', '-h']
-
-p = gdb.debug('./algo', '''
-    b *(menu+185)
-    b *(menu+200)
-    b *(menu+215)
-    b *(menu+230)
-    continue
-''')
+p = process('./algo')
 
 # p = remote('challenge.ctf.games', 30241)
 
 def buy(quality):
-    p.sendlineafter(b'> ', b'1')
-    p.sendlineafter(b'> ', str(quality).encode())
+    p.sendlineafter(b"> ", b'1')
+    p.sendlineafter(b"> ", str(quality).encode())
 
 def sell():
-    p.sendlineafter(b'> ', b'2')
-    p.sendlineafter(b'> ', b'y')
+    p.sendlineafter(b"> ", b'2')
+    p.sendlineafter(b"> ", b'y')
 
 def change(idx, quality):
-    p.sendlineafter(b'> ', b'3')
-    p.sendlineafter(b'> ', str(idx).encode())
-    p.sendlineafter(b'> ', str(quality).encode())
+    p.sendlineafter(b"> ", b'3')
+    p.sendlineafter(b"> ", str(idx).encode())
+    p.sendlineafter(b"> ", str(quality).encode())
 
 def assess():
-    p.sendlineafter(b'> ', b'4')
+    p.sendlineafter(b"> ", b'4')
 
 buy(4)
 change(0, 5)
