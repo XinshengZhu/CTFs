27e950f7daf3e4306d1c745471e05efefa2322b1
XinshengZhu <xinsheng.zhu.ny@gmail.com>
2026-08-04 02:29:51 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,17 +4,17 @@ sudo su
 
 apt-get update
 wget -q https://raw.githubusercontent.com/bata24/gef/dev/install-uv.sh -O- | sh
-echo ". /root/.gef/.venv-gef/bin/activate" >> /root/.bashrc 
-. /root/.gef/.venv-gef/bin/activate
+echo ". $HOME/.gef/.venv-gef/bin/activate" >> $HOME/.bashrc 
+. "$HOME/.gef/.venv-gef/bin/activate"
 
-export PATH="/root/.local/bin:$PATH"
+. "$HOME/.local/bin:$PATH"
 apt-get install -y pkg-config cmake build-essential
 uv pip install pwntools
 
 apt-get install -y qemu-user qemu-system
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-. $HOME/.cargo/env
+. "$HOME/.cargo/env"
 
 apt-get install -y libssl-dev liblzma-dev
 cargo install pwninit
