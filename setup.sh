eb8b3f1370c2aea155d03bd53834ab2e992ab0b5
XinshengZhu <xinsheng.zhu.ny@gmail.com>
2026-08-06 16:02:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,11 +7,11 @@ wget -q https://raw.githubusercontent.com/bata24/gef/dev/install-uv.sh -O- | sh
 echo ". $HOME/.gef/.venv-gef/bin/activate" >> $HOME/.bashrc 
 . "$HOME/.gef/.venv-gef/bin/activate"
 
+apt-get install -y qemu-user qemu-system
+
 . "$HOME/.local/bin:$PATH"
 apt-get install -y pkg-config cmake build-essential
 uv pip install pwntools
-
-apt-get install -y qemu-user qemu-system
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
